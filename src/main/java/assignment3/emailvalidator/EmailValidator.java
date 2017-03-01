ab23306461f8cc34f073f145541fd262dbc2d08a
max parsons <Max.Parsons@dal.ca>
2017-03-01 15:53:14 -0400

    Update EmailValidator for stage 2 so that tests succeed

--- src/main/java/assignment3/emailvalidator/EmailValidator.java
+++ src/main/java/assignment3/emailvalidator/EmailValidator.java
@@ -4,19 +4,23 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /*
- * Class to determine if an email is valid based on two rules:
+ * Class to determine if an email is valid based on four rules:
  * 1. the email has exactly one "@" character
  * 2. the email has at least one "." character
+ * 3. the email begins with a letter
+ * 4. the email ends with "com"
  * 
  * Uses regular expressions to check if email matches the rules
  * 
- * If the return value of the validate method is 2, then the email is valid
+ * If the return value of the validate method is 4, then the email is valid
  */
 public class EmailValidator {
 
 	//constants
 	private static final String RULE_1 = "^[^@]*(@)[^@]*$"; //email has exactly one "@"
 	private static final String RULE_2 = "\\.+"; //email has at least one "."
+	private static final String RULE_3 = "^[a-zA-Z]"; //email begins with letter
+	private static final String RULE_4 = "(com)$"; //email ends with com
 	
 	//attributes
 	private int rulesPassed;
@@ -61,6 +65,12 @@ public class EmailValidator {
 		//check if email passes rule 2
 		doesRulePass(RULE_2,email);
 		
+		//check if email passes rule 3
+		doesRulePass(RULE_3,email);
+		
+		//check if email passes rule 4
+		doesRulePass(RULE_4,email);
+		
 		return rulesPassed;
 	}
 	
@@ -76,7 +86,7 @@ public class EmailValidator {
 		setPattern(rule);
 		setMatcher(email);
 		if (matcher.find())
-		{
+		{ 
 			rulesPassed++; //rule passed
 		}
 	}
