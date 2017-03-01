bcf5de65f320b904ac415f0d4e8008fea05ab110
max parsons <Max.Parsons@dal.ca>
2017-03-01 15:35:09 -0400

    Create tests for stage 2

--- src/test/java/assignment3/emailvalidator/EmailValidatorTest.java
+++ src/test/java/assignment3/emailvalidator/EmailValidatorTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 /*
- * Stage 1 Acceptance Test:
+ * Stage 2 Acceptance Test:
  * Given an email address,
  * When the validate method is called,
  * Then the return value is the correct number of rules the email satisfies
@@ -13,43 +13,120 @@ import org.junit.Test;
 public class EmailValidatorTest {
 
 	@Test
-	/* both rules pass
-	 * meaning the email had exactly 1 "@" characters
-	 * and the email had at least 1 "." characters
+	/* all rules pass
+	 * the email had exactly 1 "@" characters
+	 * the email had at least 1 "." characters
+	 * the email began with alphabetical characters
+	 * the email ends with "com"
 	 */
-	public void testBothRulesPassed() {
+	public void testAllRulesPassed() {
 		EmailValidator validator = new EmailValidator();
-		assertEquals(2, validator.validateEmail("example@example.com"));
+		assertEquals(4, validator.validateEmail("example123@example.com"));
 	}
 	
 	@Test
-	/* only rule one passes
-	 * meaning the email had exactly 1 "@" characters
-	 * and the email had 0 "." characters
-	 */
+	//only rule one passes
 	public void testRuleOnePassedOnly() {
 		EmailValidator validator = new EmailValidator();
-		assertEquals(1, validator.validateEmail("example@examplecom"));
+		assertEquals(1, validator.validateEmail("#example@exampleca"));
 	}
 	
 	@Test
-	/* only rule two passes
-	 * meaning the email had more than 1 or 0 "@" characters
-	 * and the email had at least 1 "." characters
-	 */
+	//only rule two passes
 	public void testRuleTwoPassedOnly() {
 		EmailValidator validator = new EmailValidator();
-		assertEquals(1, validator.validateEmail("example@@example....com"));
+		assertEquals(1, validator.validateEmail("#@@666....ca"));
 	}
 	
 	@Test
-	/* no rule passes
-	 * meaning the email had more than 1 or 0 "@" characters
-	 * and the email had 0 "." characters
-	 */
+	//only rule three passes
+	public void testRuleThreePassedOnly() {
+		EmailValidator validator = new EmailValidator();
+		assertEquals(1, validator.validateEmail("example@@exampleca"));
+	}
+	
+	@Test
+	//only rule four passes
+	public void testRuleFourPassedOnly() {
+		EmailValidator validator = new EmailValidator();
+		assertEquals(1, validator.validateEmail("#examplecom"));
+	}
+	
+	@Test
+	//rules one and two pass
+	public void testRuleOneAndTwoPassed() {
+		EmailValidator validator = new EmailValidator();
+		assertEquals(2, validator.validateEmail("#example@example.ca"));
+	}
+	
+	@Test
+	//rules one and three pass
+	public void testRuleOneAndThreePassed() {
+		EmailValidator validator = new EmailValidator();
+		assertEquals(2, validator.validateEmail("example@exampleca"));
+	}
+	
+	@Test
+	//rules one and four pass
+	public void testRuleOneAndFourPassed() {
+		EmailValidator validator = new EmailValidator();
+		assertEquals(2, validator.validateEmail("#example@examplecom"));
+	}
+	
+	@Test
+	//rules two and three pass
+	public void testRuleTwoAndThreePassed() {
+		EmailValidator validator = new EmailValidator();
+		assertEquals(2, validator.validateEmail("example@@example.ca"));
+	}
+	
+	@Test
+	//rules two and four pass
+	public void testRuleTwoAndFourPassed() {
+		EmailValidator validator = new EmailValidator();
+		assertEquals(2, validator.validateEmail("#@@example.com"));
+	}
+	
+	@Test
+	//rules three and four pass
+	public void testRuleThreeAndFourPassed() {
+		EmailValidator validator = new EmailValidator();
+		assertEquals(2, validator.validateEmail("exampleexamplecom"));
+	}
+	
+	@Test
+	//only rule four fails
+	public void testRuleFourFails() {
+		EmailValidator validator = new EmailValidator();
+		assertEquals(3, validator.validateEmail("example@example.ca"));
+	}
+	
+	@Test
+	//only rule three fails
+	public void testRuleThreeFails() {
+		EmailValidator validator = new EmailValidator();
+		assertEquals(3, validator.validateEmail("#example@example.com"));
+	}
+	
+	@Test
+	//only rule two fails
+	public void testRuleTwoFails() {
+		EmailValidator validator = new EmailValidator();
+		assertEquals(3, validator.validateEmail("example@examplecom"));
+	}
+	
+	@Test
+	//only rule one fails
+	public void testRuleOneFails() {
+		EmailValidator validator = new EmailValidator();
+		assertEquals(3, validator.validateEmail("exampleexample.com"));
+	}
+	
+	@Test
+	//no rule passes
 	public void testNoRulesPassed() {
 		EmailValidator validator = new EmailValidator();
-		assertEquals(0, validator.validateEmail("exampleexamplecom"));
+		assertEquals(0, validator.validateEmail("#example@@exampleca"));
 	}
 
 }
