dd1434aa81f35b00ef4f443b6b2bfd4a4981ad17
max parsons <Max.Parsons@dal.ca>
2017-02-28 23:50:50 -0400

    Create stage 1 acceptance test

---
+++ src/test/java/assignment3/emailvalidator/EmailValidatorTest.java
@@ -0,0 +1,20 @@
+package assignment3.emailvalidator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/*
+ * Stage 1 Acceptance Test:
+ * Given a valid email address,
+ * When the validate method is called,
+ * Then the return value is 2.
+ */
+public class EmailValidatorTest {
+
+	@Test
+	public void stageOneTest() {
+		assertEquals(2, EmailValidator.validateEmail(String email));
+	}
+
+}
